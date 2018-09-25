5b18437d1ec01b5d502c9f208d643b66b8aee8e1
Test <itsistemi.test@gmail.com>
2018-09-25 10:27:23 +0200

    creting/removing sites-enabled and default config

--- provision.sh
+++ provision.sh
@@ -2,7 +2,11 @@ apt-get -y update
 
 apt-get -y install nginx
 
-sudo rm -rf /usr/share/nginx/www/
-ln -s /vagrant/www /usr/share/nginx/www
+rm -rf /etc/nginx/sites-enabled
+cp -r /vagrant/sites-enabled /etc/nginx
+
+# disabled when we have "default" config file in which we define root location (path) /vagrant/www
+# sudo rm -rf /usr/share/nginx/www/
+# ln -s /vagrant/www /usr/share/nginx/www
 
 service nginx start
